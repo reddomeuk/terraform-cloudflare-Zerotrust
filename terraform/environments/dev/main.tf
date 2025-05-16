3c181a5fcf764fc949a83e74344fe6ebdba0fc2d
reddomeuk <academy@reddome.org>
2025-05-16 16:30:09 +0100

    rls18

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -30,13 +30,20 @@ resource "cloudflare_zero_trust_access_identity_provider" "azure_ad" {
   }
 }
 
-# Create the IDP module which depends on the Azure AD provider
+# IDP Module - Must be defined first as it provides group IDs used by other modules
 module "idp" {
-  source              = "../../modules/idp"
-  account_id          = var.account_id
+  source = "../../modules/idp"
+
+  account_id = var.account_id
+  
+  # Azure AD configuration
   azure_client_id     = var.azure_client_id
   azure_client_secret = var.azure_client_secret
   azure_directory_id  = var.azure_directory_id
+  
+  # Team group IDs
+  red_team_group_ids  = var.red_team_group_ids
+  blue_team_group_ids = var.blue_team_group_ids
 }
 
 # Create the WARP module which depends on the Azure AD provider
@@ -47,13 +54,26 @@ module "warp" {
   azure_ad_provider_id = cloudflare_zero_trust_access_identity_provider.azure_ad.id
 }
 
-# Create the access module which depends on the IDP module
+# Access Module - Uses group IDs from IDP module
 module "access" {
-  source         = "../../modules/access"
-  account_id     = var.account_id
-  app_name       = "RedDome App"
-  app_domain     = "reddome.org"
-  allowed_emails = []
-  red_team_id    = module.idp.red_team_id
-  blue_team_id   = module.idp.blue_team_id
+  source = "../../modules/access"
+
+  account_id = var.account_id
+  app_name   = var.app_name
+
+  # Use the group IDs from the IDP module outputs
+  red_team_group_id  = module.idp.red_team_id
+  blue_team_group_id = module.idp.blue_team_id
+  red_team_id        = module.idp.red_team_id
+  blue_team_id       = module.idp.blue_team_id
+
+  # Azure AD configuration
+  azure_ad_provider_id = module.idp.entra_idp_id
+
+  # App domains
+  red_team_app_domain  = var.red_team_app_domain
+  blue_team_app_domain = var.blue_team_app_domain
+
+  # Email access
+  allowed_emails = var.allowed_emails
 }
