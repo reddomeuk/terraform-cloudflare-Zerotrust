d43f119cb9d2c2bfd018707f39e61c8c6a2b4217
reddomeuk <academy@reddome.org>
2025-05-16 17:07:26 +0100

    rls22

--- terraform/modules/access/main.tf
+++ terraform/modules/access/main.tf
@@ -89,16 +89,25 @@ resource "cloudflare_zero_trust_access_policy" "blue_team" {
   }
 }
 
+# Tunnel Secrets
+resource "random_id" "red_team_tunnel_secret" {
+  byte_length = 32
+}
+
+resource "random_id" "blue_team_tunnel_secret" {
+  byte_length = 32
+}
+
 # Red Team Tunnel
-resource "cloudflare_zero_trust_tunnel_cloudflared" "red_team" {
+resource "cloudflare_tunnel" "red_team" {
   account_id = var.account_id
   name       = "${var.red_team_name}-tunnel"
   secret     = random_id.red_team_tunnel_secret.b64_std
 }
 
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "red_team" {
+resource "cloudflare_tunnel_config" "red_team" {
   account_id = var.account_id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.red_team.id
+  tunnel_id  = cloudflare_tunnel.red_team.id
 
   config {
     ingress_rule {
@@ -112,15 +121,15 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "red_team" {
 }
 
 # Blue Team Tunnel
-resource "cloudflare_zero_trust_tunnel_cloudflared" "blue_team" {
+resource "cloudflare_tunnel" "blue_team" {
   account_id = var.account_id
   name       = "${var.blue_team_name}-tunnel"
   secret     = random_id.blue_team_tunnel_secret.b64_std
 }
 
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "blue_team" {
+resource "cloudflare_tunnel_config" "blue_team" {
   account_id = var.account_id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.blue_team.id
+  tunnel_id  = cloudflare_tunnel.blue_team.id
 
   config {
     ingress_rule {
@@ -131,13 +140,4 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "blue_team" {
       service = "http_status:404"
     }
   }
-}
-
-# Tunnel Secrets
-resource "random_id" "red_team_tunnel_secret" {
-  byte_length = 32
-}
-
-resource "random_id" "blue_team_tunnel_secret" {
-  byte_length = 32
 }
